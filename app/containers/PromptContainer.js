a3fddfbc909fc063f326a71347a4a25debaaf850
gabemeola <gabemeola@gmail.com>
2016-05-19 11:53:47 -0600

    refactored PromptContainer to sepearate view logic

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-var transparentBg = require("../styles").transparentBg;
+import Prompt from "../components/Prompt";
 
 var PromptContainer = React.createClass({
 	contextTypes: {
@@ -10,12 +10,12 @@ var PromptContainer = React.createClass({
 			username: ""
 		}
 	},
-	onUpdateUser: function (e) {
+	handleUpdateUser: function (e) {
 		this.setState({
 			username: e.target.value
 		})
 	},
-	onSubmitUser: function (e) {
+	handleSubmitUser: function (e) {
 		e.preventDefault();
 		var username = this.state.username;
 		this.setState({
@@ -36,28 +36,12 @@ var PromptContainer = React.createClass({
 	},
 	render(){
 		return (
-			<div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
-				<h1>{this.props.route.header}</h1>
-				<div className="col-sm-12">
-					<form onSubmit={this.onSubmitUser}>
-						<div className="form-group">
-							<input
-								className="form-control"
-							  placeholder="Github Username"
-							  onChange={this.onUpdateUser}
-							  value={this.state.username}
-							  type="text" />
-						</div>
-						<div className="form-group col-sm-4 col-sm-offset-4">
-							<button
-								className="btn btn-block btn-success"
-							  type="submit">
-									Continue
-							</button>
-						</div>
-					</form>
-				</div>
-			</div>
+			<Prompt
+				onSubmitUser={this.handleSubmitUser}
+				onUpdateUser={this.handleUpdateUser}
+			  header={this.props.route.header}
+			  username={this.state.username}
+			/>
 		)
 	}
 });
