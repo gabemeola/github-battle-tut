f149d4614277176ec83decfb66e24b257849b2a7
gabemeola <gabemeola@gmail.com>
2016-05-19 12:12:38 -0600

    Used stateless components for Prompt and added propTypes

--- app/components/Prompt.js
+++ app/components/Prompt.js
@@ -1,33 +1,39 @@
-import React from "react";
+import React, { PropTypes } from "react";
 var transparentBg = require("../styles").transparentBg;
 
-var Prompt = React.createClass({
-	render(){
-		return(
-			<div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
-				<h1>{this.props.header}</h1>
-				<div className="col-sm-12">
-					<form onSubmit={this.props.onSubmitUser}>
-						<div className="form-group">
-							<input
-								className="form-control"
-								placeholder="Github Username"
-								onChange={this.props.onUpdateUser}
-								value={this.props.username}
-								type="text" />
-						</div>
-						<div className="form-group col-sm-4 col-sm-offset-4">
-							<button
-								className="btn btn-block btn-success"
-								type="submit">
-								Continue
-							</button>
-						</div>
-					</form>
-				</div>
+// functional stateless components for only rendering UI
+function Prompt(props) {
+	return(
+		<div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
+			<h1>{props.header}</h1>
+			<div className="col-sm-12">
+				<form onSubmit={props.onSubmitUser}>
+					<div className="form-group">
+						<input
+							className="form-control"
+							placeholder="Github Username"
+							onChange={props.onUpdateUser}
+							value={props.username}
+							type="text" />
+					</div>
+					<div className="form-group col-sm-4 col-sm-offset-4">
+						<button
+							className="btn btn-block btn-success"
+							type="submit">
+							Continue
+						</button>
+					</div>
+				</form>
 			</div>
-		)
-	}
-});
+		</div>
+	)
+}
+
+Prompt.propTypes = {
+	header: PropTypes.string.isRequired,
+	onUpdateUser: PropTypes.func.isRequired,
+	onSubmitUser: PropTypes.func.isRequired,
+	username: PropTypes.string.isRequired
+};
 
 export default Prompt;
