e729c0e360df2bd8d31261405395a50b57e9070c
gabemeola <gabemeola@gmail.com>
2016-05-19 11:13:36 -0600

    added router-router home page

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
+var transparentBg = require("../styles").transparentBg;
+import { Link } from "react-router";
 
 var Home = React.createClass({
 	render(){
 		return(
-			<div>
-				Hello from Home!
+			<div className="jumbotron col-sm-12 text-center" style={transparentBg}>
+				<h1>Github Battle</h1>
+				<p className="lead">Some fancy motto</p>
+				<Link to="/playerOne">
+					<button type="button" className="btn btn-lg btn-success">Get Started</button>
+				</Link>
 			</div>
 		)
 	}
