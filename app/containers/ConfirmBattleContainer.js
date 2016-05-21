335323571e3fac5e7a1543a106b2d9b55518320e
gabemeola <gabemeola@gmail.com>
2016-05-21 12:22:22 -0600

    hooked up ConfirmBattle to route

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -2,6 +2,15 @@ import React from "react";
 import ConfirmBattle from "../components/ConfirmBattle";
 
 var ConfirmBattleContainer = React.createClass({
+	contextTypes: {
+		router: React.PropTypes.object.isRequired
+	},
+	getInitialState: function () {
+		return {
+			isLoading: true,
+			playerInfo: []
+		}
+	},
 	render(){
 		return(
 			<ConfirmBattle/>
