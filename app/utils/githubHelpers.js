86cc0fdec750db2d43fe84d54676e1cab262efff
gabemeola <gabemeola@gmail.com>
2016-05-21 13:20:10 -0600

    added axios

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -1,3 +1,13 @@
+import { axios } from "axios";
+
+var id = "YOUR_CLIENT_ID",
+		sec = "YOUR_SECRET_ID",
+		param = `?client_id=${id}&client_secret=${sec}`;
+
+function getUserInfo (username) {
+	return axios.get(`https://api.github.com/users/${username}${param}`);
+}
+
 var helpers = {
 	getPlayersInfo: function () {
 		//fetch some data from github
