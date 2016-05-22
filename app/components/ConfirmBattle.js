e7773bc765d3b8fc6f80c93d2c3c1f157c35f65e
gabemeola <gabemeola@gmail.com>
2016-05-22 15:03:19 -0600

    added UserDetailsWrapper for dynamic header

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from "react";
 import styles from "../styles";
 import { Link } from "react-router";
 import UserDetails from "./UserDetails";
+import UserDetailsWrapper from "./UserDetailsWrapper";
 
 function puke (obj) {  //Pukes the Data to the screen to look at
 	return <pre>{JSON.stringify(obj, null, " ")}</pre>;  //Pre tag keeps the Exact format you type in, tabs and all
@@ -15,14 +16,12 @@ function ConfirmBattle(props) {
 		: <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
 				<h1>Confirm Players</h1>
 				<div className='col-sm-8 col-sm-offset-2'>
-					<div className="col-sm-6">
-						<p className="lead">Player 1</p>
+					<UserDetailsWrapper header="Player One">
 						<UserDetails info={props.playersInfo[0]}/>
-					</div>
-					<div className="col-sm-6">
-						<p className="lead">Player 1</p>
+					</UserDetailsWrapper>
+					<UserDetailsWrapper header="Player Two">
 						<UserDetails info={props.playersInfo[1]}/>
-					</div>
+					</UserDetailsWrapper>
 				</div>
 				<div className='col-sm-8 col-sm-offset-2'>
 					<div className='col-sm-12' style={styles.space}>
