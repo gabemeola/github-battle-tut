96aae56f53da97f888d407787742c322bd992fff
gabemeola <gabemeola@gmail.com>
2016-05-23 11:50:22 -0600

    fixed bug and logged out props data

--- app/containers/ResultsContainer.js
+++ app/containers/ResultsContainer.js
@@ -2,9 +2,21 @@ import React from "react";
 import Results from "../components/Results";
 
 var ResultsContainer = React.createClass({
+	getInitialState: function() {
+		return {
+			isLoading: true,
+			scores: []
+		}
+	},
+	componentDidMount: function () {
+		console.log(this.props);
+	},
 	render(){
 		return(
-			<Results/>
+			<Results
+				isLoading={this.state.isLoading}
+			  scores={this.state.scores}
+			/>
 		)
 	}
 });
