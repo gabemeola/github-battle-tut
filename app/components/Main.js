acc63725cb2c613f407f3562301b8f82c72e972d
gabemeola <gabemeola@gmail.com>
2016-05-26 00:20:33 -0600

    passed down key props to child component

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,10 +1,18 @@
 import React from "react";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
 var Main = React.createClass({
 	render(){
 		return(
 			<div className="main-container">
-				{this.props.children}
+				<ReactCSSTransitionGroup
+					transitionName="appear"
+				  transitionEnterTimeout={500}
+				  transitionLeaveTimeout={500}
+				>
+					{React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+					{/* Allows to path props to otherwise none components */}
+				</ReactCSSTransitionGroup>
 			</div>
 		)
 	}
