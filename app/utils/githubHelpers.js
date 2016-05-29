ef95e673a4f2bcf6b7e75ab6ef4aca1a609ce118
gabemeola <gabemeola@gmail.com>
2016-05-29 14:06:11 -0600

    using arrow functions in githubHelpers

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -16,9 +16,7 @@ function getRepos (username) {
 
 function getTotalStars (repos) {
 	// calculate all the stars that the user has
-	return repos.data.reduce(function (prev, current) {
-		return prev + current.stargazers_count
-	}, 0)
+	return repos.data.reduce((prev, current) => prev + current.stargazers_count, 0)
 }
 
 function getPlayersData (player) {
@@ -27,12 +25,12 @@ function getPlayersData (player) {
 	// return obj with that data
 	return getRepos(player.login)
 		.then(getTotalStars)
-		.then(function (totalStars) {
-			return {
+		.then((totalStars) => (
+			{
 				followers: player.followers,
 				totalStars: totalStars
 			}
-		})
+		))
 }
 
 function calculateScores (players) {
@@ -46,15 +44,12 @@ function calculateScores (players) {
 
 export function	getPlayersInfo(players) {
 	//fetch some data from github
-	return axios.all(players.map(function (username) {
-		return getUserInfo(username);
-	})).then(function (info) {
-		return info.map(function (user) {
-			return user.data;
-		})
-	}).catch(function (err) {
-		console.warn("Error in getPlayersInfo", err);
-	})
+	return axios.all(players.map((username) => getUserInfo(username)))
+		.then((info) => info.map((user) => {
+				return user.data;
+			})
+		)
+		.catch((err) => console.warn("Error in getPlayersInfo", err))
 }
 
 export function battle(players) {
@@ -63,7 +58,5 @@ export function battle(players) {
 
 	return axios.all([playerOneData, playerTwoData])
 		.then(calculateScores)
-		.catch(function (err) {
-			console.warn("Error in getPlayersInfo: ", err)
-		})
+		.catch((err) => console.warn("Error in getPlayersInfo: ", err))
 }
