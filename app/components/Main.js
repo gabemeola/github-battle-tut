51d93725e5fba43e4c726aaa7032846bda05ecc9
gabemeola <gabemeola@gmail.com>
2016-06-01 23:11:50 -0600

    refactored main component

--- app/components/Main.js
+++ app/components/Main.js
@@ -2,20 +2,18 @@ import React from "react";
 import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 import "../main.css"
 
-const Main = React.createClass({
-	render(){
-		return(
-			<div className="main-container">
-				<ReactCSSTransitionGroup
-					transitionName="appear"
-				  transitionEnterTimeout={500}
-				  transitionLeaveTimeout={500}>
-					{React.cloneElement(this.props.children, {key: this.props.location.pathname})}
-					{/* Allows to path props to otherwise none components */}
-				</ReactCSSTransitionGroup>
-			</div>
-		)
-	}
-});
+function Main(props) {
+	return(
+		<div className="main-container">
+			<ReactCSSTransitionGroup
+				transitionName="appear"
+				transitionEnterTimeout={500}
+				transitionLeaveTimeout={500}>
+				{React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+				{/* Allows to path props to otherwise none components */}
+			</ReactCSSTransitionGroup>
+		</div>
+	)
+}
 
 export default Main;
