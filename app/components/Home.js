27a567b5bcf645ad9e5942b987c3467defde96be
gabemeola <gabemeola@gmail.com>
2016-06-01 23:08:50 -0600

    refactored Home component

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,19 +2,16 @@ import React from "react";
 import { Link } from "react-router";
 import MainContainer from "../containers/MainContainer";
 
-
-const Home = React.createClass({
-	render(){
-		return(
-			<MainContainer>
-				<h1>Github Battle</h1>
-				<p className="lead">Some fancy motto</p>
-				<Link to="/playerOne">
-					<button type="button" className="btn btn-lg btn-success">Get Started</button>
-				</Link>
-			</MainContainer>
-		)
-	}
-});
+function Home(props) {
+	return(
+		<MainContainer>
+			<h1>Github Battle</h1>
+			<p className="lead">Some fancy motto</p>
+			<Link to="/playerOne">
+				<button type="button" className="btn btn-lg btn-success">Get Started</button>
+			</Link>
+		</MainContainer>
+	)
+}
 
 export default Home;
